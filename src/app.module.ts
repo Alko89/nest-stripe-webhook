722afd7f3765f7012d2422bd67033fe812a5dd70
Alko89 <alefer89@gmail.com>
2022-04-23 14:03:51 +0200

    Stripe webhook

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,18 @@
+import { StripeModule } from '@golevelup/nestjs-stripe';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { stripeSecretKey, stripeWebhookSecret } from './env';
 
 @Module({
-  imports: [],
+  imports: [
+    StripeModule.forRoot(StripeModule, {
+      apiKey: stripeSecretKey,
+      webhookConfig: {
+        stripeWebhookSecret: stripeWebhookSecret,
+      },
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,15 @@
+import { StripeWebhookHandler } from '@golevelup/nestjs-stripe';
 import { Injectable } from '@nestjs/common';
+import Stripe from 'stripe';
 
 @Injectable()
 export class AppService {
   getHello(): string {
     return 'Hello World!';
+  }
+
+  @StripeWebhookHandler('*')
+  handlePaymentIntentCreated(evt: Stripe.Event) {
+    console.log(evt);
   }
 }
